d957d377e5b6262769a8d218b6de83a765df8aac
Parle648 <sashahoumi@gmail.com>
2022-10-28 11:09:45 +0300

    init: add title + responcive & add block userApi

--- app.js
+++ app.js
@@ -22,3 +22,54 @@ navSelects.forEach(ShowSelects);
 ShowSelects(langSelect);
 
 ShowSelects(phoneSelect);
+// main slider
+const sliderDots = document.querySelectorAll('.dots');
+const sliderBacground = document.querySelectorAll('.background-title');
+
+sliderDots.forEach(changeSliderDots);
+
+function changeSliderDots(el){
+    el.onclick = function(){
+        sliderDots.forEach(function(el){
+            el.classList.remove('active');
+        })
+        el.classList.add('active');
+
+        sliderBacground.forEach((element) => {
+            element.classList.remove('active-background');
+            if(element.getAttribute('data-number') === el.getAttribute('data-number')){
+                element.classList.add('active-background');
+            }
+        })
+    };
+}
+
+setInterval(changeBacground, 10000);
+
+function changeBacground(){
+    let activeSlide = document.querySelector('.active-background'),
+        activeDot = document.querySelector('.active');
+
+    changeClass(activeSlide, 'active-background');
+    changeClass(activeDot, 'active');
+
+    if(activeSlide.getAttribute('data-number') === 'five'){
+        sliderBacground.forEach(function(el){
+            el.classList.remove('active-background');
+            if(el.getAttribute('data-number') === 'one'){
+                el.classList.add('active-background')
+            }
+        })
+        sliderDots.forEach(function(el){
+            el.classList.remove('active')
+            if(el.getAttribute('data-number') === 'one'){
+                el.classList.add('active')
+            }
+        })
+    }
+}
+
+function changeClass(activeElem, classes){
+    activeElem.classList.remove(classes);
+    activeElem.nextElementSibling.classList.add(classes);
+}
